f71f1e0268cfc231ed5773dd502f454e2aebadf9
Bartul <bgrbin@tvz.hr>
2020-02-09 14:48:08 +0100

    map changes

--- maps/map.w3x/war3map.lua
+++ maps/map.w3x/war3map.lua
@@ -1,5 +1,12 @@
 gg_rct_FloorA = nil
 gg_rct_FloorB = nil
+gg_rct_Room1 = nil
+gg_rct_Room2 = nil
+gg_rct_Room3 = nil
+gg_rct_Room4 = nil
+gg_rct_Room5 = nil
+gg_rct_Room6 = nil
+gg_rct_RoomBoss = nil
 gg_trg_Melee_Initialization = nil
 function InitGlobals()
 end
@@ -10,8 +17,9 @@ function CreateUnitsForPlayer0()
     local unitID
     local t
     local life
-    u = BlzCreateUnitWithSkin(p, FourCC("Hpal"), -1257.3, 1741.9, 86.410, FourCC("Hpal"))
+    u = BlzCreateUnitWithSkin(p, FourCC("Hpal"), -2567.2, -6814.1, 86.410, FourCC("Hpal"))
     u = BlzCreateUnitWithSkin(p, FourCC("hcth"), -2176.6, 547.8, 312.153, FourCC("hcth"))
+    u = BlzCreateUnitWithSkin(p, FourCC("Hpal"), -1423.2, 1822.8, 233.181, FourCC("Hpal"))
 end
 
 function CreateNeutralPassive()
@@ -39,7 +47,14 @@ end
 function CreateRegions()
     local we
     gg_rct_FloorA = Rect(2048.0, -7168.0, 6656.0, -2560.0)
-    gg_rct_FloorB = Rect(-3072.0, -7168.0, 1536.0, -2528.0)
+    gg_rct_FloorB = Rect(-3104.0, -7168.0, 1504.0, -2528.0)
+    gg_rct_Room1 = Rect(-2208.0, -7200.0, -1024.0, -6144.0)
+    gg_rct_Room2 = Rect(-1024.0, -7168.0, 160.0, -6112.0)
+    gg_rct_Room3 = Rect(128.0, -7168.0, 1312.0, -5792.0)
+    gg_rct_Room4 = Rect(128.0, -5792.0, 1280.0, -4416.0)
+    gg_rct_Room5 = Rect(-32.0, -4384.0, 1408.0, -2816.0)
+    gg_rct_Room6 = Rect(-2144.0, -3712.0, 0.0, -2688.0)
+    gg_rct_RoomBoss = Rect(-2592.0, -5248.0, -640.0, -3712.0)
 end
 
 function Trig_Melee_Initialization_Actions()
@@ -82,8 +97,8 @@ function main()
     SetCameraBounds(-3328.0 + GetCameraMargin(CAMERA_MARGIN_LEFT), -7680.0 + GetCameraMargin(CAMERA_MARGIN_BOTTOM), 7424.0 - GetCameraMargin(CAMERA_MARGIN_RIGHT), 3072.0 - GetCameraMargin(CAMERA_MARGIN_TOP), -3328.0 + GetCameraMargin(CAMERA_MARGIN_LEFT), 3072.0 - GetCameraMargin(CAMERA_MARGIN_TOP), 7424.0 - GetCameraMargin(CAMERA_MARGIN_RIGHT), -7680.0 + GetCameraMargin(CAMERA_MARGIN_BOTTOM))
     SetDayNightModels("Environment\\DNC\\DNCLordaeron\\DNCLordaeronTerrain\\DNCLordaeronTerrain.mdl", "Environment\\DNC\\DNCLordaeron\\DNCLordaeronUnit\\DNCLordaeronUnit.mdl")
     NewSoundEnvironment("Default")
-    SetAmbientDaySound("LordaeronSummerDay")
-    SetAmbientNightSound("LordaeronSummerNight")
+    SetAmbientDaySound("CityScapeDay")
+    SetAmbientNightSound("CityScapeNight")
     SetMapMusic("Music", true, 0)
     CreateRegions()
     CreateAllUnits()
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import { addScriptHook, MapPlayer, Timer, Unit } from 'w3ts';
+import { FireMageTalentTree } from 'Classes/FireMageTalentTree';
+import { TalentTreeView } from 'UI/TalentTree/TalentTreeView';
+import { TalentTree } from 'UI/TalentTree/TalentTree';
 
 function tsMain() {
   const unit = new Unit(MapPlayer.fromIndex(0), FourCC("hfoo"), 0, 0, 270);
   unit.name = "TypeScript";
 
+  TalentTreeView.init();
+
+  TimerStart(CreateTimer(), 1.5, false, () => {
+    let tree = new FireMageTalentTree(gg_unit_Hpal_0001, "0");
+    TalentTreeView.SetPlayerViewedTree(Player(0), tree);
+  });
+  new Timer().start(1.0, false, () => {
+      // KillUnit(gg_unit_Hpal_0001);
+      // let tree = new FireMageTalentTree(gg_unit_Hpal_0001);
+      
+  });
+
   new Timer().start(1.00, true, () => {
-    unit.color = ConvertPlayerColor(math.random(0, bj_MAX_PLAYERS));
+    // unit.color = ConvertPlayerColor(math.random(0, bj_MAX_PLAYERS));
+
+    // 
+    // TalentTreeView.SetPlayerViewedTree(Player(0), tree);
   });
 
   print("Welcome to TypeScript!");
