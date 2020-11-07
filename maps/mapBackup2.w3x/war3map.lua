2faa4e99538056a85855334a5eee7935f8f7c956
Bartul <bgrbin@tvz.hr>
2020-11-07 17:48:09 +0100

    map changes

---
+++ maps/mapBackup2.w3x/war3map.lua
@@ -0,0 +1,191 @@
+gg_trg_Initialization = nil
+gg_unit_Hblm_0003 = nil
+gg_unit_Hpal_0002 = nil
+function InitGlobals()
+end
+
+function CreateAllItems()
+    local itemID
+    BlzCreateItemWithSkin(FourCC("prvt"), 515.6, -1278.1, FourCC("prvt"))
+    BlzCreateItemWithSkin(FourCC("prvt"), 574.3, -1309.8, FourCC("prvt"))
+    BlzCreateItemWithSkin(FourCC("prvt"), 644.0, -1303.2, FourCC("prvt"))
+    BlzCreateItemWithSkin(FourCC("prvt"), 699.0, -1296.5, FourCC("prvt"))
+    BlzCreateItemWithSkin(FourCC("prvt"), 364.3, -1240.2, FourCC("prvt"))
+end
+
+function CreateUnitsForPlayer0()
+    local p = Player(0)
+    local u
+    local unitID
+    local t
+    local life
+    gg_unit_Hblm_0003 = BlzCreateUnitWithSkin(p, FourCC("Hblm"), 643.3, -1100.9, 3.210, FourCC("Hblm"))
+    u = BlzCreateUnitWithSkin(p, FourCC("hkni"), 318.1, -587.8, 263.878, FourCC("hkni"))
+    u = BlzCreateUnitWithSkin(p, FourCC("hkni"), 473.3, -668.6, 128.445, FourCC("hkni"))
+    u = BlzCreateUnitWithSkin(p, FourCC("hkni"), 255.3, -1044.2, 42.398, FourCC("hkni"))
+    u = BlzCreateUnitWithSkin(p, FourCC("hkni"), 1253.3, -733.2, 128.445, FourCC("hkni"))
+    u = BlzCreateUnitWithSkin(p, FourCC("hpea"), 102.4, -1249.2, 206.264, FourCC("hpea"))
+end
+
+function CreateUnitsForPlayer1()
+    local p = Player(1)
+    local u
+    local unitID
+    local t
+    local life
+    gg_unit_Hpal_0002 = BlzCreateUnitWithSkin(p, FourCC("Hpal"), 237.9, -832.4, 269.898, FourCC("Hpal"))
+    u = BlzCreateUnitWithSkin(p, FourCC("hkni"), 90.3, -515.9, 70.084, FourCC("hkni"))
+    u = BlzCreateUnitWithSkin(p, FourCC("hkni"), -28.5, -684.0, 208.977, FourCC("hkni"))
+    u = BlzCreateUnitWithSkin(p, FourCC("hkni"), 480.1, -902.3, 84.872, FourCC("hkni"))
+end
+
+function CreatePlayerBuildings()
+end
+
+function CreatePlayerUnits()
+    CreateUnitsForPlayer0()
+    CreateUnitsForPlayer1()
+end
+
+function CreateAllUnits()
+    CreatePlayerBuildings()
+    CreatePlayerUnits()
+end
+
+--
+function Trig_Initialization_Actions()
+    SelectUnitForPlayerSingle(gg_unit_Hblm_0003, Player(0))
+    SelectUnitForPlayerSingle(gg_unit_Hpal_0002, Player(1))
+end
+
+function InitTrig_Initialization()
+    gg_trg_Initialization = CreateTrigger()
+    TriggerAddAction(gg_trg_Initialization, Trig_Initialization_Actions)
+end
+
+function InitCustomTriggers()
+    InitTrig_Initialization()
+end
+
+function RunInitializationTriggers()
+    ConditionalTriggerExecute(gg_trg_Initialization)
+end
+
+function InitCustomPlayerSlots()
+    SetPlayerStartLocation(Player(0), 0)
+    ForcePlayerStartLocation(Player(0), 0)
+    SetPlayerColor(Player(0), ConvertPlayerColor(0))
+    SetPlayerRacePreference(Player(0), RACE_PREF_HUMAN)
+    SetPlayerRaceSelectable(Player(0), false)
+    SetPlayerController(Player(0), MAP_CONTROL_USER)
+    SetPlayerStartLocation(Player(1), 1)
+    ForcePlayerStartLocation(Player(1), 1)
+    SetPlayerColor(Player(1), ConvertPlayerColor(1))
+    SetPlayerRacePreference(Player(1), RACE_PREF_ORC)
+    SetPlayerRaceSelectable(Player(1), false)
+    SetPlayerController(Player(1), MAP_CONTROL_USER)
+    SetPlayerStartLocation(Player(2), 2)
+    ForcePlayerStartLocation(Player(2), 2)
+    SetPlayerColor(Player(2), ConvertPlayerColor(2))
+    SetPlayerRacePreference(Player(2), RACE_PREF_UNDEAD)
+    SetPlayerRaceSelectable(Player(2), false)
+    SetPlayerController(Player(2), MAP_CONTROL_USER)
+    SetPlayerStartLocation(Player(3), 3)
+    ForcePlayerStartLocation(Player(3), 3)
+    SetPlayerColor(Player(3), ConvertPlayerColor(3))
+    SetPlayerRacePreference(Player(3), RACE_PREF_NIGHTELF)
+    SetPlayerRaceSelectable(Player(3), false)
+    SetPlayerController(Player(3), MAP_CONTROL_USER)
+    SetPlayerStartLocation(Player(11), 4)
+    ForcePlayerStartLocation(Player(11), 4)
+    SetPlayerColor(Player(11), ConvertPlayerColor(11))
+    SetPlayerRacePreference(Player(11), RACE_PREF_NIGHTELF)
+    SetPlayerRaceSelectable(Player(11), false)
+    SetPlayerController(Player(11), MAP_CONTROL_COMPUTER)
+end
+
+function InitCustomTeams()
+    SetPlayerTeam(Player(0), 0)
+    SetPlayerTeam(Player(1), 0)
+    SetPlayerTeam(Player(2), 0)
+    SetPlayerTeam(Player(3), 0)
+    SetPlayerAllianceStateAllyBJ(Player(0), Player(1), true)
+    SetPlayerAllianceStateAllyBJ(Player(0), Player(2), true)
+    SetPlayerAllianceStateAllyBJ(Player(0), Player(3), true)
+    SetPlayerAllianceStateAllyBJ(Player(1), Player(0), true)
+    SetPlayerAllianceStateAllyBJ(Player(1), Player(2), true)
+    SetPlayerAllianceStateAllyBJ(Player(1), Player(3), true)
+    SetPlayerAllianceStateAllyBJ(Player(2), Player(0), true)
+    SetPlayerAllianceStateAllyBJ(Player(2), Player(1), true)
+    SetPlayerAllianceStateAllyBJ(Player(2), Player(3), true)
+    SetPlayerAllianceStateAllyBJ(Player(3), Player(0), true)
+    SetPlayerAllianceStateAllyBJ(Player(3), Player(1), true)
+    SetPlayerAllianceStateAllyBJ(Player(3), Player(2), true)
+    SetPlayerAllianceStateVisionBJ(Player(0), Player(1), true)
+    SetPlayerAllianceStateVisionBJ(Player(0), Player(2), true)
+    SetPlayerAllianceStateVisionBJ(Player(0), Player(3), true)
+    SetPlayerAllianceStateVisionBJ(Player(1), Player(0), true)
+    SetPlayerAllianceStateVisionBJ(Player(1), Player(2), true)
+    SetPlayerAllianceStateVisionBJ(Player(1), Player(3), true)
+    SetPlayerAllianceStateVisionBJ(Player(2), Player(0), true)
+    SetPlayerAllianceStateVisionBJ(Player(2), Player(1), true)
+    SetPlayerAllianceStateVisionBJ(Player(2), Player(3), true)
+    SetPlayerAllianceStateVisionBJ(Player(3), Player(0), true)
+    SetPlayerAllianceStateVisionBJ(Player(3), Player(1), true)
+    SetPlayerAllianceStateVisionBJ(Player(3), Player(2), true)
+    SetPlayerTeam(Player(11), 1)
+end
+
+function InitAllyPriorities()
+    SetStartLocPrioCount(0, 3)
+    SetStartLocPrio(0, 0, 1, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrio(0, 1, 2, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrio(0, 2, 3, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrioCount(1, 3)
+    SetStartLocPrio(1, 0, 0, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrio(1, 1, 2, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrio(1, 2, 3, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrioCount(2, 3)
+    SetStartLocPrio(2, 0, 0, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrio(2, 1, 1, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrio(2, 2, 3, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrioCount(3, 3)
+    SetStartLocPrio(3, 0, 0, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrio(3, 1, 1, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrio(3, 2, 2, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrioCount(4, 3)
+    SetStartLocPrio(4, 0, 0, MAP_LOC_PRIO_HIGH)
+    SetStartLocPrio(4, 1, 2, MAP_LOC_PRIO_HIGH)
+end
+
+function main()
+    SetCameraBounds(-3328.0 + GetCameraMargin(CAMERA_MARGIN_LEFT), -3584.0 + GetCameraMargin(CAMERA_MARGIN_BOTTOM), 3328.0 - GetCameraMargin(CAMERA_MARGIN_RIGHT), 3072.0 - GetCameraMargin(CAMERA_MARGIN_TOP), -3328.0 + GetCameraMargin(CAMERA_MARGIN_LEFT), 3072.0 - GetCameraMargin(CAMERA_MARGIN_TOP), 3328.0 - GetCameraMargin(CAMERA_MARGIN_RIGHT), -3584.0 + GetCameraMargin(CAMERA_MARGIN_BOTTOM))
+    SetDayNightModels("Environment\\DNC\\DNCLordaeron\\DNCLordaeronTerrain\\DNCLordaeronTerrain.mdl", "Environment\\DNC\\DNCLordaeron\\DNCLordaeronUnit\\DNCLordaeronUnit.mdl")
+    NewSoundEnvironment("Default")
+    SetAmbientDaySound("LordaeronSummerDay")
+    SetAmbientNightSound("LordaeronSummerNight")
+    SetMapMusic("Music", true, 0)
+    CreateAllItems()
+    CreateAllUnits()
+    InitBlizzard()
+    InitGlobals()
+    InitCustomTriggers()
+    RunInitializationTriggers()
+end
+
+function config()
+    SetMapName("TRIGSTR_001")
+    SetMapDescription("TRIGSTR_003")
+    SetPlayers(5)
+    SetTeams(5)
+    SetGamePlacement(MAP_PLACEMENT_TEAMS_TOGETHER)
+    DefineStartLocation(0, 128.0, -896.0)
+    DefineStartLocation(1, 128.0, -896.0)
+    DefineStartLocation(2, 128.0, -896.0)
+    DefineStartLocation(3, 128.0, -896.0)
+    DefineStartLocation(4, 128.0, -896.0)
+    InitCustomPlayerSlots()
+    InitCustomTeams()
+    InitAllyPriorities()
+end
+
